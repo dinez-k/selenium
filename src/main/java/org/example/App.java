0faf0bfbec60215133394da36edfddbb39f30b3c
LAPTOP-7GBCGNF8\dines <dineshspm18@gmail.com>
2024-05-14 20:24:04 +0530

    initial commit

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,37 @@
+package org.example;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main(String[] args)
+    {
+
+        System.out.println( "Hello World!" );
+
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("http://www.google.com");
+        WebElement textArea = driver.findElement(By.name("q"));
+
+        //input text , number , password , textarea
+        //radio , checkbox
+        //dropdown
+
+        // Input text into the textarea
+        textArea.sendKeys("selenium.dev");
+
+        textArea.sendKeys(Keys.ENTER);
+
+
+
+    }
+}
