1a8360facdc87bd7b93b757ee6e5b57bb457d2a6
DESKTOP-CE2SJRL\91968 <dineshjohnds@gmail.com>
2024-05-15 19:58:02 +0530

    latest chanes

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,39 +1,39 @@
 package org.example;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 /**
  * Hello world!
+ //input text , number , password , textarea
+//radio , checkbox
+//dropdown
  *
  */
 public class App 
 {
     public static void main(String[] args)
     {
-
         System.out.println( "Hello World!" );
 
-        WebDriver driver = new ChromeDriver();
+        WebDriver driver = new ChromeDriver( );
 
         driver.get("http://www.google.com");
+
         WebElement textArea = driver.findElement(By.name("q"));
 
-        //input text , number , password , textarea
-        //radio , checkbox
-        //dropdown
-
-        //changes
-
-        // Input text into the textarea
-        textArea.sendKeys("selenium.dev");
-
+        textArea.sendKeys("demoqa.com");
         textArea.sendKeys(Keys.ENTER);
 
+        WebElement lab = driver.findElement(By.tagName("https://demoqa.com/"));
 
+        lab.click();
+
+        WebElement label = driver.findElement(By.name("z9PoV"));
+        label.click();
+
+        WebElement label1 = driver.findElement(By.name("Elements"));
+        label1.click();
 
     }
 }
